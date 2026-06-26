5cbfaa2226309ef6296478851be8fd7aa929eefb
Chandrahas10 <chandrahaskori9@gmail.com>
2026-06-26 16:19:13 +0530

    Normal Dependency without circular

---
+++ CircularDependency/src/main/java/org/example/Orderservice.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Orderservice {
+    private  Paymentservice paymentservice;
+
+    public  Orderservice(Paymentservice paymentservice){
+        this.paymentservice =paymentservice;
+    }
+
+    public  void placeOrder(){
+        paymentservice.pay();
+
+        System.out.println("Order is placed");
+    }
+}
