cb47d67a4edc634445704baf16477b438e26c075
Chandrahas10 <chandrahaskori9@gmail.com>
2026-06-26 16:57:54 +0530

    Circular Dependency in Spring Boot  bcoz ot bad practice of code

--- CircularDependency/src/main/java/org/example/Orderservice.java
+++ CircularDependency/src/main/java/org/example/Orderservice.java
@@ -1,14 +1,16 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Orderservice {
+    @Autowired
     private  Paymentservice paymentservice;
 
-    public  Orderservice(Paymentservice paymentservice){
-        this.paymentservice =paymentservice;
-    }
+//    public  Orderservice(Paymentservice paymentservice){
+//        this.paymentservice =paymentservice; //bcoz of constructor
+//    }
 
     public  void placeOrder(){
         paymentservice.pay();
--- CircularDependency/src/main/java/org/example/Paymentservice.java
+++ CircularDependency/src/main/java/org/example/Paymentservice.java
@@ -1,15 +1,17 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Paymentservice {
 
+    @Autowired
     Orderservice orderservice ;
 
-    public  Paymentservice(Orderservice orderservice){
-        this.orderservice=orderservice;
-    }
+//    public  Paymentservice(Orderservice orderservice){
+//        this.orderservice=orderservice;
+//    }
 
     public  void pay(){
         System.out.println("Payment is done");
